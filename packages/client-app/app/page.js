1ebfe7f6dbd25f4915656ce4708fc6b8387ac64a
Nija Kukadiya <nijakukadiya467@gmail.com>
2024-06-14 12:28:42 +0530

    fixed

--- packages/client-app/app/page.js
+++ packages/client-app/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="text-center text-3xl pt-8">
       <h1>Welcome to Client App</h1>
-      
+      <p>client teting app</p>
     </div>
   );
 }
